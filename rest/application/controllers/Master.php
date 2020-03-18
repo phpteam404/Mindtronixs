57eb8eff084ddd4fa6574bc802445cc6d39f3873
phpteam404 <57086837+phpteam404@users.noreply.github.com>
2020-03-18 15:13:42 +0530

    Syntax error fixed

--- rest/application/controllers/Master.php
+++ rest/application/controllers/Master.php
@@ -95,7 +95,7 @@ class Master extends REST_Controller
         }
         // $data = tableOptions($data);
         // print_r($this->session_user_info->user_role_id);exit;
-        if($data['master_key']=='ticket_status')){
+        if($data['master_key']=='ticket_status'){
             if($this->session_user_info->user_role_id==1){
                 $data['master_ids']=array(46,47,48);
             }

b777217e0937a65b02bbedc6f42fbbcb59c28224
phpteam404 <php@thresholdsoft.com>
2020-04-13 15:57:46 +0530

    online user registration

--- rest/application/controllers/Externalcontroller.php
+++ rest/application/controllers/Externalcontroller.php
@@ -1,7 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 error_reporting(0);
-
 class Externalcontroller extends CI_Controller
 {
     
@@ -40,6 +39,16 @@ class Externalcontroller extends CI_Controller
                     $documents[$k]['document_url']=DOCUMENT_PATH.'digitalcontent/'.$v['document_name'];
                 }
             }
+
+            $content_history = array(
+                'content_id' => $data['digital_content_management_id'],
+                'session_user_id' => !empty($_GET['user_id'])?$_GET['user_id']:0,
+                'client_browser' => $_SERVER['HTTP_USER_AGENT'],
+                'client_os' => getUserOS($_SERVER['HTTP_USER_AGENT']),
+                'client_remote_address' => $_SERVER['REMOTE_ADDR'],
+                'created_date' => currentDate()
+                );
+            $this->User_model->insert_data('digital_content_view_history',$content_history);
             $content_info[0]['documents']=!empty($documents)?$documents:array();
             $result = array('status'=>TRUE, 'message' => $this->lang->line('success'), 'data'=>array('data' => $content_info));
             echo json_encode($result);exit;
@@ -53,26 +62,54 @@ class Externalcontroller extends CI_Controller
 
 
     //* online user registration start *//
-    // public function addOnlineUser(){
-    //     $data=$this->input->post();
-    //     $user_table_data=array(
-    //         'first_name'=>!empty($data['name'])?$data['name']:'',
-    //         'last_name'=>!empty($data['last_name'])?$data['last_name']:' ',
-    //         'email'=>!empty($data['email'])?$data['email']:'',
-    //         'password'=>!empty($data['password'])?md5($data['password']):'',
-    //         'user_role_id'=>9,
-    //         'phone_no'=>!empty($data['phone_no'])?$data['phone_no']:'',
-    //         'created_by'=>0,
-    //         'created_on'=>currentDate()
-    //     );
-    //     $user_id=$this->User_model->inset_data('user',$user_table_data);
-    //     $student_table_data=array(
-    //         'user_id'=>$user_id,
-    //         'grade'=>!empty($data['grade'])?$data['grade']:'',
-    //         'parent'=>!empty($data['parent_name'])?$data['parent_name']:'',
-    //         'lead_source'=>!empty($data['parent_name'])?$data['parent_name']:'',
-            
-    //     );
-    // }
+    public function addOnlineUser(){
+        $data=$this->input->post();
+        // $data = json_decode(file_get_contents("php://input"), true);
+        // print_r($data);exit;
+        if(empty($data)){
+            $result = array('status'=>FALSE,'message'=>$this->lang->line('invalid_data'),'data'=>'1');
+            echo json_encode($result); exit;
+        }
+        if(!empty($data['email'])){
+            $email_check = $this->User_model->check_email(array('email' => $data['email']));
+            if(!empty($email_check)){
+                $result = array('status'=>FALSE,'error'=>array('email' => $this->lang->line('email_duplicate')),'data'=>'');
+                echo json_encode($result); exit;
+            }
+        }
+        $user_table_data=array(
+            'first_name'=>!empty($data['name'])?$data['name']:'',
+            'last_name'=>!empty($data['last_name'])?$data['last_name']:' ',
+            'email'=>!empty($data['email'])?$data['email']:'',
+            'password'=>!empty($data['password'])?md5($data['password']):'',
+            'user_role_id'=>9,
+            'phone_no'=>!empty($data['number'])?$data['phone_no']:'',
+            'created_by'=>0,
+            'created_on'=>currentDate()
+        );
+        $user_id=$this->User_model->insert_data('user',$user_table_data);
+        $student_table_data=array(
+            'user_id'=>$user_id,
+            'grade'=>!empty($data['grade'])?$data['grade']:'',
+            'parent'=>!empty($data['parentname'])?$data['parentname']:'',
+            'lead_source'=>!empty($data['leadsource'])?$data['leadsource']:'',
+            'franchise_fee_id'=>!empty($data['fee_structure_id'])?$data['fee_structure_id']:0,
+            'created_by'=>0,
+            'created_on'=>currentDate(),
+            'relation_with_student'=>!empty($data['relation'])?$data['relation']:0
+        );
+        $student_id=$this->User_model->insert_data('student',$student_table_data);
+
+        if($user_id>0 && $student_id){
+            $result = array('status'=>TRUE, 'message' => "online User Created Scucessfully", 'data'=>array('data' =>''));
+            echo json_encode($result);exit;
+        }
+        else{
+            $result = array('status'=>FALSE,'message'=>$this->lang->line('invalid_data'),'data'=>'2');
+            echo json_encode($result); exit;
+        }
+
+        
+    }
     //* online user registration end *//
 }

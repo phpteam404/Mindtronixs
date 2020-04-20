c042babef37e8f19a8160258a8b08fbfc7a8fb75
phpteam404 <php@thresholdsoft.com>
2020-04-20 15:47:11 +0530

    invoice all chages

--- rest/application/controllers/GeneratePdfController.php
+++ rest/application/controllers/GeneratePdfController.php
@@ -20,12 +20,12 @@ class GeneratePdfController extends REST_Controller {
         if(!empty($data['student_invoice_id']) || $data['onlineuser_inoive_id']){// to get student/online user information to generate pdf
             $student_invoice_data=$this->Invoices_model->getStudentInvoiceInfo(array('student_invoice_id'=>!empty($data['student_invoice_id'])?$data['student_invoice_id']:$data['onlineuser_inoive_id'])); 
             $invoice_number=!empty($student_invoice_data[0]['invoice_number'])?$student_invoice_data[0]['invoice_number']:'';
-            $amount=!empty($student_invoice_data[0]['amount'])?$student_invoice_data[0]['amount']:0;
+            $amount=!empty($student_invoice_data[0]['amount'])?number_format($student_invoice_data[0]['amount']):0;
             $tax=!empty($student_invoice_data[0]['tax'])?$student_invoice_data[0]['tax']:0;
-            $tax_amount=!empty($student_invoice_data[0]['tax_amount'])?$student_invoice_data[0]['tax_amount']:0;
+            $tax_amount=!empty($student_invoice_data[0]['tax_amount'])?number_format($student_invoice_data[0]['tax_amount']):0;
             $discount=!empty($student_invoice_data[0]['discount'])?$student_invoice_data[0]['discount']:0;
-            $discount_amount=!empty($student_invoice_data[0]['discount_amount'])?$student_invoice_data[0]['discount_amount']:0;
-            $total_amount=!empty($student_invoice_data[0]['total_amount'])?$student_invoice_data[0]['total_amount']:0;
+            $discount_amount=!empty($student_invoice_data[0]['discount_amount'])?number_format($student_invoice_data[0]['discount_amount']):0;
+            $total_amount=!empty($student_invoice_data[0]['total_amount'])?number_format($student_invoice_data[0]['total_amount']):0;
             $invoice_date=!empty($student_invoice_data[0]['invoice_date'])?$student_invoice_data[0]['invoice_date']:'';
             $due_date=!empty($student_invoice_data[0]['due_date'])?$student_invoice_data[0]['due_date']:'';
             $user_name=!empty($student_invoice_data[0]['user_name'])?$student_invoice_data[0]['user_name']:'';
@@ -39,12 +39,12 @@ class GeneratePdfController extends REST_Controller {
         if(!empty($data['school_invoice_id'])){// to get school information to generate pdf
             $school_invoice_data=$this->Invoices_model->getSchoolInvoiceInfo(array('school_invoice_id'=>$data['school_invoice_id']));
             $invoice_number=!empty($school_invoice_data[0]['invoice_number'])?$school_invoice_data[0]['invoice_number']:'';
-            $amount=!empty($school_invoice_data[0]['amount'])?$school_invoice_data[0]['amount']:0;
+            $amount=!empty($school_invoice_data[0]['amount'])?number_format($school_invoice_data[0]['amount']):0;
             $tax=!empty($school_invoice_data[0]['tax'])?$school_invoice_data[0]['tax']:0;
-            $tax_amount=!empty($school_invoice_data[0]['tax_amount'])?$school_invoice_data[0]['tax_amount']:0;
+            $tax_amount=!empty($school_invoice_data[0]['tax_amount'])?number_format($school_invoice_data[0]['tax_amount']):0;
             $discount=!empty($school_invoice_data[0]['discount'])?$school_invoice_data[0]['discount']:0;
-            $discount_amount=!empty($school_invoice_data[0]['discount_amount'])?$school_invoice_data[0]['discount_amount']:0;
-            $total_amount=!empty($school_invoice_data[0]['total_amount'])?$school_invoice_data[0]['total_amount']:0;
+            $discount_amount=!empty($school_invoice_data[0]['discount_amount'])?number_format($school_invoice_data[0]['discount_amount']):0;
+            $total_amount=!empty($school_invoice_data[0]['total_amount'])?number_format($school_invoice_data[0]['total_amount']):0;
             $invoice_date=!empty($school_invoice_data[0]['invoice_date'])?$school_invoice_data[0]['invoice_date']:'';
             $user_name=!empty($school_invoice_data[0]['user_name'])?$school_invoice_data[0]['user_name']:'';
             $email=!empty($school_invoice_data[0]['email'])?$school_invoice_data[0]['email']:'';
@@ -60,10 +60,10 @@ class GeneratePdfController extends REST_Controller {
             $invoice_number=!empty($franchise_invoice_data[0]['invoice_number'])?$franchise_invoice_data[0]['invoice_number']:'';
             $amount=!empty($franchise_invoice_data[0]['amount'])?$franchise_invoice_data[0]['amount']:0;
             $tax=!empty($franchise_invoice_data[0]['tax'])?$franchise_invoice_data[0]['tax']:0;
-            $tax_amount=!empty($franchise_invoice_data[0]['tax_amount'])?$franchise_invoice_data[0]['tax_amount']:0;
+            $tax_amount=!empty($franchise_invoice_data[0]['tax_amount'])?number_format($franchise_invoice_data[0]['tax_amount']):0;
             $discount=!empty($franchise_invoice_data[0]['discount'])?$franchise_invoice_data[0]['discount']:0;
-            $discount_amount=!empty($franchise_invoice_data[0]['discount_amount'])?$franchise_invoice_data[0]['discount_amount']:0;
-            $total_amount=!empty($franchise_invoice_data[0]['total_amount'])?$franchise_invoice_data[0]['total_amount']:0;
+            $discount_amount=!empty($franchise_invoice_data[0]['discount_amount'])?number_format($franchise_invoice_data[0]['discount_amount']):0;
+            $total_amount=!empty($franchise_invoice_data[0]['total_amount'])?number_format($franchise_invoice_data[0]['total_amount']):0;
             $invoice_date=!empty($franchise_invoice_data[0]['invoice_date'])?$franchise_invoice_data[0]['invoice_date']:'';
             $due_date=!empty($franchise_invoice_data[0]['due_date'])?$franchise_invoice_data[0]['due_date']:'';
             $user_name=!empty($franchise_invoice_data[0]['franchise_name'])?$franchise_invoice_data[0]['franchise_name']:'';
@@ -89,12 +89,12 @@ class GeneratePdfController extends REST_Controller {
                 .main-head-leftblock{
                     float: left;
                     display:inline-block;
-                    width:50%;
+                    width:45%;
                 }
                 .main-head-rightblock{
                      text-align: right;
                      float:left;
-                     width: 50%;
+                     width: 55%;
                 }
                 .clearfix::after {
                     content: "";
@@ -118,11 +118,13 @@ class GeneratePdfController extends REST_Controller {
                     padding: 5px;
                     width: 100px;
                 }
-                .total-invoice-list tr td:first-child{
-                    width: 200px;
-                }
+              
                 .total-invoice-list tr td:last-child{
                     border-bottom:1px solid #cccccc;
+                }
+                
+                .total-invoice-list tr td:first-child{
+                    width: 250px;
                 }
             
                 </style>
@@ -142,7 +144,7 @@ class GeneratePdfController extends REST_Controller {
             </div>
             <div class="clearfix">
                 <div class="main-head-leftblock">
-                    <h2 style="margin: 0;color: #000000;border-bottom:1px solid #cccccc;font-size: 16px;">BILL TO</h2>
+                    <h2 style="margin: 0;color: #000000;border-bottom:1px solid #cccccc;font-size: 16px;">Bill to</h2>
                     <p style="margin: 5px 0px 0px;">'.$user_name.'</p>
                     <p style="margin: 5px 0px 0px;">'.$address.'</p>
                     <p style="margin: 5px 0px 0px;">'.$email.'</p>
@@ -154,15 +156,15 @@ class GeneratePdfController extends REST_Controller {
                    <p><span style="width: 100px;text-align: right;">Due Date: &nbsp;</span><span></span></p> -->
                    <table style="width:100%">
                        <tr>
-                           <td style="text-align: right;">Invoice No:</td>
+                           <td style="text-align: right;width:120px;">Invoice No:</td>
                            <td style="text-align: right;">'.$invoice_number.'</td>
                        </tr>
                        <tr>
-                           <td style="text-align: right;">Invoice Date:</td>
+                           <td style="text-align: right;width:120px;">Invoice Date:</td>
                            <td style="text-align: right;">'.$invoice_date.'</td>
                         </tr>
                         <tr>
-                            <td style="text-align: right;">Due Date:</td>
+                            <td style="text-align: right;width:120px;">Due Date:</td>
                             <td style="text-align: right;">'.$due_date.'</td>
                         </tr>
                    </table>
@@ -269,7 +271,7 @@ class GeneratePdfController extends REST_Controller {
                             <td>'.$amount.'</td>
                           </tr>
                           <tr>
-                            <td>Discount</td>
+                            <td>Discount(%)</td>
                             <td>'.$discount.'</td>
                           </tr>
                           <tr>
@@ -277,11 +279,11 @@ class GeneratePdfController extends REST_Controller {
                             <td>'.$discount_amount.'</td>
                           </tr>
                           <tr>
-                            <td>Tax rate</td>
+                            <td>Tax(%)</td>
                             <td>'.$tax.'</td>
                           </tr>
                           <tr>
-                            <td>Total tax</td>
+                            <td>Tax amount</td>
                             <td>'.$tax_amount.'</td>
                           </tr>
                           <tr>
@@ -318,10 +320,15 @@ class GeneratePdfController extends REST_Controller {
         // $dompdf->stream("downloads/".$filename.".pdf", array("Attachment" => 1));
         $output = $dompdf->output();
         file_put_contents('downloads/'.$filename.".pdf", $output);
-        $result = array('status'=>TRUE, 'message' => $this->lang->line('success'), 'data'=>array('filename'=>$filename,'file_url'=>WEB_BASE_URL.'rest/downloads/'.$filename.'.pdf'));
-        $this->response($result, REST_Controller::HTTP_OK);
+        if(!empty($invoice_number)){
+            $result = array('status'=>TRUE, 'message' => $this->lang->line('success'), 'data'=>array('filename'=>$filename,'file_url'=>WEB_BASE_URL.'rest/downloads/'.$filename.'.pdf'));
+            $this->response($result, REST_Controller::HTTP_OK);
+        }
+        else{
+            $result = array('status'=>FALSE,'error'=>$this->lang->line('invalid_data'),'data'=>'1');
+            $this->response($result, REST_Controller::HTTP_OK);
+        }
     }
-
     //* generating pdf for all invoices(student/) end *//
 }
 ?>
